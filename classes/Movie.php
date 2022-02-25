3cd925bbfec1bd0edceb5198a48e9d41f31d05dd
Matteo Pompei <datxizzan@gmail.com>
2022-02-25 17:28:50 +0100

    Funzione creata

--- classes/Movie.php
+++ classes/Movie.php
@@ -1,17 +1,21 @@
 <?php
 
 class Movie {
-  public $titolo;
-  public $genere;
-  public $anno;
-  public $regista;
-  public $cast;
+  private $titolo;
+  private $genere;
+  private $anno;
+  private $regista;
+  private $cast;
 
   public function __construct($_titolo, $_genere, $_anno, $_regista, $_cast) {
     $this->titolo = $_titolo;
-    $this->generale = $_genere;
+    $this->genere = $_genere;
     $this->anno = $_anno;
     $this->regista = $_regista;
     $this->cast = $_cast;
+  }
+
+  public function getInfo() {
+    return "{$this->titolo} {$this->genere} {$this->anno} {$this->regista} {$this->cast}";
   }
 }
